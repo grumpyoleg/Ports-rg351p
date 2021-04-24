e93a046afd40a5a0c617ec183580bde3efc9b2a4
grumpyoleg <42551096+grumpyoleg@users.noreply.github.com>
2021-04-24 22:14:04 +0200

    Update blobby.sh

--- blobby.sh
+++ blobby.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/Blob"
+LIBDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberPorts/lib64"
+BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberPorts/bin"
 
-LD_LIBRARY_PATH=.
-exec bin/blobby_exec
+# system
+export LD_LIBRARY_PATH=$GAMEDIR/lib:$LIBDIR:/usr/lib
+
+cd $GAMEDIR
+
+./blobby.sh -fullscreen
